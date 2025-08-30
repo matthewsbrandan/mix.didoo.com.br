b4284d383cf0788fa6c850c0444bf3f115809d8b
Mateus Brandão <mateusfleria@gmail.com>
2025-08-30 17:27:16 -0300

    feat: Finalizado layout atualizado

--- views/sections/products_alt.blade.php
+++ views/sections/products_alt.blade.php
@@ -8,32 +8,27 @@
       {{ innerStyleIssetAttr('font-size', $products->title, 'size') }}
     @endisset
   ">{{ $products->title->text ?? 'Produtos Personalizados' }}</h1>
-  <div class="container-categories d-flex">
-    <div class="d-flex" style="gap: .5rem; flex-direction: column;">
+  <div class="container-categories">
+    <div class="d-flex" style="gap: .5rem; flex-direction: column; padding: 1rem;">
       @foreach($products->categories as $category)
         <button
           type="button"
           class="btn btn-danger btn-sm text-uppercase btn-filter-category"
           style="font-weight: 500;align-self: flex-start; margin: 0 1.6rem; border-radius: 2rem; padding-left: 1rem; padding-right: 1rem;"
-          onclick="handleFilterProductCategory($(this))"
         >{{ $category }}</button>
-        <div class="row pt-3 mb-5 mx-0" id="produtos" style="overflow-x: auto;">
+        <div class="d-flex pt-3 mb-5 mx-0" id="produtos" style="overflow-x: auto; gap: 2rem;">
           @foreach(array_filter($products->items, fn($prod_item) => $prod_item->category === $category) as $prod_item)
             @php $product_url = $internal ? route('internal_product.show', ['slug' => $prod_item->slug]) : route('product.show', ['slug' => $prod_item->slug]); @endphp
             <div
-              class="col-xl-4 col-lg-4 col-md-4 col-sm-6 mb-3 product-item {{
-                isset($products->columns) && $products->columns != 1 ? (
-                  $products->columns == 2 ? 'col-6':(
-                    $products->columns == 3 ? 'col-4':''
-                  )
-                ):''
-              }}"
+              class="product-item"
+              style="width: 16.25rem;"
               data-category="{{ $prod_item->category ?? null }}"
               data-name="{{ $prod_item->title->text ?? null }}"
               data-code="{{ $prod_item->code ?? null }}"
               data-slug="{{ $prod_item->slug ?? null }}"
             >
-              <div class="product-item-container border h-100 p-3 rounded d-flex flex-column" style="
+              <div class="product-item-container border h-100 p-2 d-flex flex-column" style="
+                border-radius: 2rem;
                 @isset($prod_item->styles)
                   {{ innerStyleIssetAttr('background', $prod_item->styles, 'background', '#ffffffff') }}
                   {{ innerStyleIssetAttr('border-color', $prod_item->styles, 'border_color', '#ffffffff', null, true) }}
@@ -44,25 +39,25 @@
                   <div class="product-item-image" onclick='handleShowMultiPhotos({!! json_encode($prod_item) !!}, {!! $internal ? 'true':'false' !!})'>
                     <img
                       src="{{ $prod_item->image->src ?? '' }}"
-                      class="rounded w-100"
+                      style="border-radius: 1.6rem; width: 15rem; height: 15rem; object-fit: cover;"
                       alt="{{ $prod_item->image->alt ?? $prod_item->title->text ?? 'Imagem do produto' }}"
                     />
                   </div>
                 </div>
-                <div class="product-container pt-3">
-                  <div class="product-item-name text-center mb-1">
+                <div class="product-container pt-2">
+                  <div class="product-item-name">
                     <a
                       class="product-item-image-link"
                       href="{{ $product_url }}"
                       target="_blank"
                     >
-                      <strong class="text-uppercase h5" style="
-                        {{ innerStyleIssetAttr('font-size', $prod_item->title, 'fontsize') }}
-                      ">{{ $prod_item->title->text }}</strong>
+                      <strong
+                        class="truncate-2"
+                        style="{{ innerStyleIssetAttr('font-size', $prod_item->title, 'fontsize') }}">{{ $prod_item->title->text }}</strong>
                     </a>
                   </div>
                   @isset($prod_item->description)
-                    <p style="font-size: .9rem;">
+                    <p class="truncate-1" style="font-size: .9rem; opacity: 0.9;">
                       {{ $prod_item->description }}
                     </p>
                   @endisset
@@ -73,6 +68,9 @@
                     class="btn btn-danger btn-block"
                     onclick='handleShowMultiPhotos({!! json_encode($prod_item) !!},{!! $internal ? 'true':'false' !!})'
                     style="
+                      width: 100%;
+                      border-radius: 1.2rem;
+                      font-size: .9rem;
                       {{ innerStyleIssetAttr('background', $prod_item->button, 'background') }}
                       {{ innerStyleIssetAttr('color', $prod_item->button, 'color') }}
                     "

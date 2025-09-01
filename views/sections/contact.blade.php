f2d6c360c903417c1ba4305a1458355908bd26d6
Mateus Brandão <mateusfleria@gmail.com>
2025-08-31 21:40:48 -0300

    feat: Icone do Twitter

--- views/sections/contact.blade.php
+++ views/sections/contact.blade.php
@@ -140,7 +140,19 @@
                 @endisset
                 @isset($code->twitter)
                   <a href="{{ $code->twitter }}" target="_blank">
-                    <i class="fab fa-twitter fa-2x"></i>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      aria-labelledby="iconTwitterTitle"
+                      version="1.1" id="X-_icon" x="0px" y="0px" viewBox="4 4 24 24"
+                      width="24" height="24"
+                    >
+                      <g style="fill: currentColor;">
+                        <polygon points="14.202 16.292 14.788 17.15 9.241 23.58 7.91 23.58" style=""/>
+                        <polygon points="22.968 7.672 17.028 14.559 16.437 13.7 21.641 7.672" style=""/>
+                        <path d="M 12.369 8.509 L 22.132 22.743 L 19.133 22.743 L 9.37 8.509 L 12.369 8.509 M 12.809 7.672 L 7.78 7.672 L 18.692 23.58 L 23.722 23.58 L 12.809 7.672 Z" style=""/>
+                      </g>
+                    </svg>
                   </a> &nbsp; &nbsp;
                 @endisset
                 @isset($code->youtube)
